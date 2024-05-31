979bbbdb91b62702ec0e9a4691116402977c1620
ranjeetraj444 <ranjeetraj44666@gmail.com>
2024-05-31 13:11:46 +0530

    updated backend server

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { data2 } from "./data.js";
+
 
 export function getData(setLoading, seData) {
 	// console.log(data2.Customer_Estimate_Flow, "hello");
 	setLoading(true);
-	axios("/sample-data")
+	axios("https://boxigo-backend.onrender.com/api/sample-data")
 		.then((data) => {
 			// console.log(data);
 			// console.log(data.data.Customer_Estimate_Flow);
@@ -12,9 +12,7 @@ export function getData(setLoading, seData) {
 			setLoading(false);
 		})
 		.catch((error) => {
-			seData(data2.Customer_Estimate_Flow);
 			alert(error);
-			console.log(error);
 		})
 		.finally(() => {
 			setLoading(false);
